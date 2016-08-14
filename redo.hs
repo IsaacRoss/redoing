42562404da4f7b37cdeb5b2ef47d6554631b4dc5
Isaac Ross <isaac253@gmail.com>
2016-08-14 19:54:53 -0400

    init

---
+++ redo.hs
@@ -0,0 +1,14 @@
+import System.Environment (getArgs)
+import System.Process (createProcess, waitForProcess, shell)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  mapM_ redo args
+
+
+redo :: String -> IO ()
+redo target = do
+  (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do"
+  _ <- waitForProcess ph
+  return ()
